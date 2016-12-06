76bb7748438e1385ce2b66c9cdd2d87545cd29b2
fendouai <fendouai@gmail.com>
2016-12-06 14:58:00 +0800

    add valid example

--- example/ValidCodeExample.cs
+++ example/ValidCodeExample.cs
@@ -22,8 +22,8 @@ namespace example
 
             // 验证 API
             // API文档地址 http://docs.jiguang.cn/jsms/server/rest_api_jsms/#api_2
-            string msg_id = "27c8295f-3764-45b7-8c2b-f15047082c89";
-            ValidPayload codes = new ValidPayload("909190");
+            string msg_id = "227effab-b00c-4569-8021-220a036b7ae6";
+            ValidPayload codes = new ValidPayload("121806");
             String codesjson = codes.ToJson(codes);
             Console.WriteLine(codesjson);
             client._SMSClient.validCodes(codes,msg_id);
